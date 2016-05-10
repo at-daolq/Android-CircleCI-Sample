42cdc253974948d23a764ea7615f5262c2bd17b9
Dao Le Quang <daolq@asiantech.vn>
2016-05-10 14:46:13 +0700

    change circle.yml & test

--- app/src/main/java/vn/asiantech/com/android_circleci_sample/MainActivity.java
+++ app/src/main/java/vn/asiantech/com/android_circleci_sample/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public int A;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
